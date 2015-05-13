bec64055a93736e3bc0449fe28698deb6dc06676
alervd <alervd@users.noreply.github.com>
2015-05-13 22:53:32 +0300

    - vmsUnzip bug.

--- archive/vmsUnZip.h
+++ archive/vmsUnZip.h
@@ -27,22 +27,35 @@ public:
 					if (UNZ_OK == unzGetCurrentFileInfo(zipFile, &fi,
 						filename, sizeof(filename), 0, 0, 0, 0))
 					{
-						if (UNZ_OK == unzOpenCurrentFile(zipFile))
+						if (*filename && (filename [strlen (filename) - 1] == '/' || filename [strlen (filename) - 1] == '\\'))
+						{
+							assert (fi.uncompressed_size == 0);
+							tstring tstr = ptszDstFolder;
+							tstr += '\\';
+							tstr += CA2TEX<> (filename);
+							tstr += '1';
+							vmsBuildPathToFile (tstr.c_str ());
+							result = TRUE;
+						}
+						else if (UNZ_OK == unzOpenCurrentFile(zipFile))
 						{
 							UINT dataLen = fi.uncompressed_size;
-							BYTE* fileData = new BYTE[dataLen];
-							if (!fileData)
-								break;
-							if (dataLen == unzReadCurrentFile(zipFile, fileData, dataLen))
+							std::vector <BYTE> fileData (dataLen ? dataLen : 1);
+							if (dataLen == unzReadCurrentFile(zipFile, &fileData.front (), dataLen))
 							{
 								char filePathName[MAX_PATH] = { 0 };
 								strcat(filePathName, T2A((LPTSTR)ptszDstFolder));
 								strcat(filePathName, "\\");
 								strcat(filePathName, filename);
 								FILE* pFile = fopen(filePathName, "wb");
+								if (!pFile)
+								{
+									vmsBuildPathToFile (CA2TEX<> (filePathName));
+									pFile = fopen (filePathName, "wb");
+								}
 								if (pFile)
 								{
-									result = (dataLen == fwrite(fileData, 1, dataLen, pFile));
+									result = (dataLen == fwrite(&fileData.front (), 1, dataLen, pFile));
 									result = result && (0 == fclose(pFile));
 								}
 								else
@@ -53,9 +66,8 @@ public:
 								if (!result)
 									error = std::error_code(errno, std::generic_category());
 							}
-							delete[] fileData;
+							result = result && (UNZ_OK == unzCloseCurrentFile(zipFile));
 						}
-						result = result && (UNZ_OK == unzCloseCurrentFile(zipFile));
 					}
 					if (!result)
 						break;
